156cafb591f19271350729c4aca4873afd78e4f5
Daniel Enriquez <daniel.enm17@gmail.com>
2024-03-17 15:11:45 -0600

    Improved setup.sh completion messages

--- setup.sh
+++ setup.sh
@@ -4,5 +4,10 @@ SCRIPT_DIR="$(pwd)/main.sh"
 chmod +x $SCRIPT_DIR
 echo "source $SCRIPT_DIR" >> ~/.zshrc
 
-echo "Added source $SCRIPT_DIR to ~/.zshrc"
-echo "Call quick_commit_ai to use the script"
+# Last messages to user:
+printf "\033[0;32m------------------------------------------------------\033[0m\n"
+printf "\033[1;32mSetup Complete! ✅\033[0m\n"
+printf "Added: source $SCRIPT_DIR to: ~/.zshrc\n"
+printf "\033[0;36mYou can consider adding aliases like:\033[0m\n"
+printf "\033[0;36mgac=alias quick_commit_ai --commit\033[0m\n"
+printf "\033[0;36mgas=alias quick_commit_ai --squash\033[0m\n"
